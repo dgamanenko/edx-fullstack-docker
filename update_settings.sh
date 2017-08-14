ae0fcee649ffa6d8def679da19ea6db4a8241698
dgamanenko <dgamanenko@gmail.com>
2017-08-14 13:26:44 +0000

    Update update_settings.sh

--- update_settings.sh
+++ update_settings.sh
@@ -6,6 +6,9 @@ PREVIEW_URL=preview-docker-fullstack.raccoongang.com
 
 EMAIL_HOST_USER='username'
 EMAIL_HOST_PASSWORD='password'
+EMAIL_HOST: "email-smtp.eu-west-1.amazonaws.com"
+EMAIL_PORT: 587
+EMAIL_USE_TLS: true
 
 find -type f -name '*.json' | xargs sed -i 's/edx.fullstack.nginx:18000/'$LMS_URL'/g'
 find -type f -name '*.json' | xargs sed -i 's/http:\/\/'$LMS_URL'/https:\/\/'$LMS_URL'/g'
@@ -17,4 +20,6 @@ EMAIL_HOST_USER=$(echo ${EMAIL_HOST_USER} | sed 's:/:\\\/:g')
 EMAIL_HOST_PASSWORD=$(echo ${EMAIL_HOST_PASSWORD} | sed 's:/:\\\/:g')
 find -type f -name '*.json' | xargs sed -i 's/"EMAIL_HOST_PASSWORD": "",/"EMAIL_HOST_PASSWORD": "'$EMAIL_HOST_PASSWORD'",/g'
 find -type f -name '*.json' | xargs sed -i 's/"EMAIL_HOST_USER": "",/"EMAIL_HOST_USER": "'$EMAIL_HOST_USER'",/g'
+find -type f -name '*.json' | xargs sed -i 's/"EMAIL_HOST": "email-smtp.eu-west-1.amazonaws.com",/"EMAIL_HOST": "'$EMAIL_HOST'",/g'
+find -type f -name '*.json' | xargs sed -i 's/"EMAIL_PORT": 587,/"EMAIL_PORT": "'$EMAIL_PORT'",/g'
 
